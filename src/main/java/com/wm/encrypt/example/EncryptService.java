1a42202f8b7ef90f12526e6e1c0be1aec0d24c5c
ZeroWM <wm4968@gmail.com>
2022-10-08 17:49:31 +0800

    feat(ecrypt): add info for method

--- src/main/java/com/wm/encrypt/example/EncryptService.java
+++ src/main/java/com/wm/encrypt/example/EncryptService.java
@@ -5,6 +5,8 @@ import org.mindrot.jbcrypt.BCrypt;
 public class EncryptService {
 
   public static  String encrypt(String content){
+    //content abc1234234234
+    //salt $2a$10$fHCUFTep1eMSLegmFuMRZ.
     return  BCrypt.hashpw(content, BCrypt.gensalt());
   }
 
@@ -14,6 +16,8 @@ public class EncryptService {
   }
 
   public static boolean check(String input, String hashContent){
+    //input "abc1234234234"
+    //hashContent "$2a$10$yEG9bFE5GIN3VteFzJUpKOhg8I.w7BtTE40iwrcfyP40B7VkDW9Ea"
     return  BCrypt.checkpw(input, hashContent);
   }
 
